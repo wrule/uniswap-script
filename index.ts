6790dba39ee1a53743a39911018c677bdbe28570
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-03 18:01:37 +0800

    连接断点

--- index.ts
+++ index.ts
@@ -1,4 +1,12 @@
 
 import { Pool } from '@uniswap/v3-sdk'
+import { ethers } from 'ethers';
 
-console.log('你好，世界');
+const secret = require('./.secret.json');
+
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider(`https://optimism-mainnet.infura.io/v3/${secret.prj_id}`);
+  console.log('你好，世界');
+}
+
+main();
