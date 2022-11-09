0b9dd524d562eefd3ae14cb717ddb72a34c8d88b
jimao <jimao@jimaodeMBP.lan>
2022-11-09 22:05:36 +0800

    轮训更新价格

--- index.ts
+++ index.ts
@@ -62,8 +62,11 @@ async function main() {
   const poolAddress = '0x85149247691df622eaF1a8Bd0CaFd40BC45154a9';
   const pool = new UniswapV3Pool(poolAddress, IUniswapV3PoolABI, provider, signer);
   await pool.Update();
-  console.log(`1${pool.Pool.token0.symbol} = ${pool.Pool.token0Price.toFixed()}${pool.Pool.token1.symbol}`);
-  console.log(`1${pool.Pool.token1.symbol} = ${pool.Pool.token1Price.toFixed()}${pool.Pool.token0.symbol}`);
+  setInterval(async () => {
+    await pool.UpdateState();
+    console.log(`1${pool.Pool.token0.symbol} = ${pool.Pool.token0Price.toFixed()}${pool.Pool.token1.symbol}`);
+    console.log(`1${pool.Pool.token1.symbol} = ${pool.Pool.token1Price.toFixed()}${pool.Pool.token0.symbol}`);
+  }, 1000);
 }
 
 main();
