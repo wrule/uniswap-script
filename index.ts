8ae2c9431a84e9a90cbd4b090e7766e063bde70d
jimao <jimao@jimaodeMBP.lan>
2022-11-09 22:36:29 +0800

    报价员合约交互

--- index.ts
+++ index.ts
@@ -10,15 +10,24 @@ async function main() {
   const signer = provider.getSigner();
   const poolAddress = '0x85149247691df622eaF1a8Bd0CaFd40BC45154a9';
   const pool = new UniswapV3Pool(poolAddress, IUniswapV3PoolABI, provider, signer);
+  await pool.Update();
 
   const quoterContract = new ethers.Contract('0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6', QuoterABI, provider);
+  const amountIn = 1;
+  const quotedAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
+    pool.Immutables.token0,
+    pool.Immutables.token1,
+    pool.Immutables.fee,
+    amountIn.toString(),
+    0,
+  );
+  console.log(quotedAmountOut.toString());
 
-  await pool.Update();
-  setInterval(async () => {
-    await pool.UpdateState();
-    console.log(`1${pool.Pool.token0.symbol} = ${pool.Pool.token0Price.toFixed()}${pool.Pool.token1.symbol}`);
-    console.log(`1${pool.Pool.token1.symbol} = ${pool.Pool.token1Price.toFixed()}${pool.Pool.token0.symbol}`);
-  }, 1000);
+  // setInterval(async () => {
+  //   await pool.UpdateState();
+  //   console.log(`1${pool.Pool.token0.symbol} = ${pool.Pool.token0Price.toFixed()}${pool.Pool.token1.symbol}`);
+  //   console.log(`1${pool.Pool.token1.symbol} = ${pool.Pool.token1Price.toFixed()}${pool.Pool.token0.symbol}`);
+  // }, 1000);
 }
 
 main();
